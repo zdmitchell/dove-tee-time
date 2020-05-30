31da77a987359ed639aa68c8cfba0591506314aa
Zachary Mitchell <zachary592@cox.net>
2020-05-30 15:43:02 -0700

    Navigation: open particular day's tee sheet
    Let's add three more configurations in our `settings.json` file. A day
    and month to book the tee time for and a earliest time to book for that
    day.
    
    To get to the particular day's tee sheet we're looking for, we need to
    first find the month date picker for the month we specified. If that
    month isn't found on the page, we print an error and exit.
    
    Similarly, once we find a month we look for the day we specified. If
    that day isn't a selectable day, we print an error and exit. If it is,
    we click it to open that day's tee sheet.

--- get_tee_time.py
+++ get_tee_time.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import json
+from datetime import date, time
+from calendar import month_name
 
 fname = 'settings.json'
 delay = 10 # seconds max wait
@@ -16,6 +18,11 @@ except FileNotFoundError:
 with settings_file:
    settings = json.load(settings_file)
 
+today = date.today()
+
+day_to_book = date(today.year, settings['month'], settings['day'])
+earliest_time = time(settings['earliestTime'], 0)
+
 browser = webdriver.Firefox()
 browser.get("https://web.foretees.com/v5/servlet/LoginPrompt?cn=dovecanyonclub")
 
@@ -26,3 +33,33 @@ browser.find_element_by_css_selector('#login input[type="submit"]').submit()
 
 WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'topnav_item')))
 browser.get("https://web.foretees.com/v5/dovecanyonclub_golf_m0/Member_select")
+
+calendar_months = browser.find_elements_by_class_name('ui-datepicker-inline')
+
+for month in calendar_months:
+   title = month.find_element_by_class_name('ui-datepicker-title')
+
+   if title.find_element_by_class_name('ui-datepicker-month').get_attribute("textContent") == month_name[day_to_book.month]:
+      month_to_find = month
+      break
+
+try:
+   month_to_find
+except NameError:
+   print("Can't book tee time for month you specified")
+   exit()
+
+selectable_days = month.find_elements_by_css_selector('tbody a.ui-state-default')
+
+for calendar_day in selectable_days:
+   if int(calendar_day.get_attribute("textContent")) == day_to_book.day:
+      day_link = calendar_day
+      break
+
+try:
+   day_link
+except NameError:
+   print("Cannot make tee times for the day you specified")
+   exit()
+
+day_link.click()
