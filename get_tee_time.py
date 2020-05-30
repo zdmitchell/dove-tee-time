8760e557ef572829ac0cae0565002501fc11646e
Zachary Mitchell <zachary592@cox.net>
2020-05-30 13:37:36 -0700

    Navigation: go to Make a Tee Time
    After logging in, a login confirmation appears for a couple seconds
    until the user is redirected. To go to the page we want, we first need
    to wait for this redirection. For no particular reason, let's wait for
    the `.topnav_item` element to be present before going to the page we
    need.

--- get_tee_time.py
+++ get_tee_time.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import json
 
 fname = 'settings.json'
+delay = 10 # seconds max wait
 
 try:
    settings_file = open(fname)
@@ -18,3 +22,7 @@ browser.get("https://web.foretees.com/v5/servlet/LoginPrompt?cn=dovecanyonclub")
 browser.find_element_by_id('user_name').send_keys(settings['username'])
 browser.find_element_by_id('password').send_keys(settings['password'])
 browser.find_element_by_css_selector('#login input[type="submit"]').submit()
+
+
+WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'topnav_item')))
+browser.get("https://web.foretees.com/v5/dovecanyonclub_golf_m0/Member_select")
