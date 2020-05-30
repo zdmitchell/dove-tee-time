eb0b4367706ebefb293677060ac42456a02a4647
Zachary Mitchell <zachary592@cox.net>
2020-05-30 12:25:48 -0700

    Login: use settings.json
    Let's add an untracked file called `settings.json` to store the username
    and password that we will use to login.
    
    Since we need the file for the user's username and password, let's print
    an error message and exit if the file doesn't exist.
    
    To login, we select the username and password fields by id and fill
    them. Then we find the submit button within the login form and submit
    it.

--- get_tee_time.py
+++ get_tee_time.py
@@ -1,4 +1,20 @@
 from selenium import webdriver
+import json
+
+fname = 'settings.json'
+
+try:
+   settings_file = open(fname)
+except FileNotFoundError:
+   print("File %s not found" % fname)
+   exit()
+
+with settings_file:
+   settings = json.load(settings_file)
 
 browser = webdriver.Firefox()
 browser.get("https://web.foretees.com/v5/servlet/LoginPrompt?cn=dovecanyonclub")
+
+browser.find_element_by_id('user_name').send_keys(settings['username'])
+browser.find_element_by_id('password').send_keys(settings['password'])
+browser.find_element_by_css_selector('#login input[type="submit"]').submit()
